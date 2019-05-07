8eb057a5c492a5618ab035a7a8d55ecdb277d8b8
Minh Huy Le <s181335@students.aie.edu.au>
2019-05-08 08:53:30 +1000

    Architect Prototype

--- Assets/Scripts/pokoro/Architecture.cs
+++ Assets/Scripts/pokoro/Architecture.cs
@@ -1,79 +1,133 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace pokoro.Architecture
 {
 
-    public class Architecture : MonoBehaviour {}
+    public class Architecture : MonoBehaviour { }
+
+    public class LevelDesigner
+    {
+        //Editor based class to assist in designing the level
+        StudentManager studentManager;
+
+        public class Gun
+        {
+            public void Shoot() { }
+        }
+        public class RandomAudioPlayer
+        {
+            public void Play() { }
+        }
+
+        public class LevelDesignAssistance : MonoBehaviour
+        //Alt names: LevelDesigner, LevelDesignTools, LevelDesignUtility
+        {
+            //Editor based class to assist in designing the level
+
+        }
+
+        public class StudentUtility : ScriptableObject
+        {
+            //Alternative names:
+            //StudentBuilder, StudentManager
+
+            //Class that manages, handles creation and randomization of students
+
+            [SerializeField] Classroom classroom;
+            [SerializeField] GameObject studentPrefab;
+            Student[] students;
+            public int rows = 2, columns = 4;
+            public Rect posNpad = new Rect(0, 0, 20f, 20f);     //Position (x,y) and Padding (widht, height)
 
 
-	public class Gun
-	{
-		public void Shoot() {}
-	}
-	public class RandomAudioPlayer
-	{
-		public void Play() {}
-	}
+            public static void GenerateStudents()
+            {
+                //Create a new empty classroom game object on the root node
+                var newClassroom = new GameObject("Classroom");
+                newClassroom.transform.SetParent(null);
 
-	public class LevelDesignAssistance : MonoBehaviour
-		//Alt names: LevelDesigner, LevelDesignTools, LevelDesignUtility
-	{
-		//Editor based class to assist in designing the level
-		
-	}
+                //Create students from prefabs and child them to the new classroom
 
-	public class StudentUtility : ScriptableObject
-	{
-		//Alternative names:
-		//StudentBuilder, StudentManager
+                //Set the student positions relative to the starting point
 
-		//Class that manages, handles creation and randomization of students
+            }
+        }
 
-		[SerializeField] Classroom classroom;
-		[SerializeField] GameObject studentPrefab;
-		Student[] students;
-		public int rows = 2, columns = 4;
-		public Rect posNpad = new Rect(0, 0, 20f, 20f);		//Position (x,y) and Padding (widht, height)
+        public class Student
+        {
+            StudentAppearance appearance;
 
+            void Awake()
+            {
+                studentManager = GameObject.FindObjectOfType<StudentManager>();
+            }
+        }
 
-		public static void GenerateStudents()
-		{
-			//Create a new empty classroom game object on the root node
-			var newClassroom = new GameObject("Classroom");
-			newClassroom.transform.SetParent(null);
-
-			//Create students from prefabs and child them to the new classroom
-
-			//Set the student positions relative to the starting point
-		
-		}
-	
-	
-	}
-
-
-
-	public class Student
-	{
-		StudentAppearance appearance;
-		
-		
-
-	}
-
-
-	struct StudentAppearance
-	{
-		// Sprite desk;
-		// Sprite head;
-		// Sprite hair;
-		// Sprite 
-	}
+        public class GameManager
+        {
+            StudentManager studentManager;
 
 
 
-	// public class 
+        }
+
+        public enum StudentType
+        {
+            Athletic,
+            Skater,
+            Lovers,
+            HallMonitor,
+            ChatterBox
+        }
+
+        public enum PassDirection
+        {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+        struct StudentAppearance
+        {
+            // Sprite desk;
+            // Sprite head;
+            // Sprite hair;
+            // Sprite 
+        }
+
+        public class Student
+        {
+            public PassDirection[] passDirections = new PassDirection[4];   //U, D, L, R
+            public StudentType studentType;
+
+        }
 
 
+        public class StudentManager : MonoBehaviour
+        {
+            //Class that holds all the students
+            //Build students?
+            //Singleton?
 
+            public Student[] students;
+
+            void Awake()
+            {
+                students = GameObject.FindObjectsOfType(typeof(Student)) as Student[];
+            }
+        }
+
+
+        public class InputController
+        {
+            public bool left;
+            public bool right;
+            public bool up;
+            public bool down;
+            public bool next;
+            public bool prev;
+        }
+    }
 }
---
+++ Assets/Scripts/pokoro/ArchitectureEditor.cs
@@ -0,0 +1,83 @@
+﻿﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace pokoro.Architecture
+{
+
+    public class Architecture : MonoBehaviour {}
+
+    public class LevelDesigner
+    {
+        //Editor based class to assist in designing the level
+        StudentManager studentManager;
+
+    public class StudentManager
+        void Awake()
+        {
+            studentManager = GameObject.FindObjectOfType<StudentManager>();
+        }
+    }
+
+    public class GameManager
+    {
+        //Class that handles all students
+        StudentManager studentManager;
+
+
+        
+    }
+
+    public class LevelDesigner
+    public enum StudentType
+    {
+        //Editor based class to assist in designing the level
+        Athletic,
+        Skater,
+        Lovers,
+        HallMonitor,
+        ChatterBox
+    }
+
+    public enum PassDirection
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public class 
+    public class Student
+    {
+        public PassDirection[] passDirections = new PassDirection[4];    //U, D, L, R
+        public StudentType studentType;
+        
+    }
+
+
+    public class StudentManager : MonoBehaviour
+    {
+        //Class that holds all the students
+        //Build students?
+
+        //Singleton?
+
+        public Student[] students;
+
+        void Awake()
+        {
+            students = GameObject.FindObjectsOfType(typeof(Student)) as Student[];
+        }
+    }
+
+
+    public class InputController
+    {
+        public bool left;
+        public bool right;
+        public bool up;
+        public bool down;
+        public bool next;
+        public bool prev;
+    }
